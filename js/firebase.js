7eb9a2ead17ec6faa4978a32575bb653faad0508
Jonah Waigwa Mugi <44112284+jonah03@users.noreply.github.com>
2021-09-28 12:43:10 +0300

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBbHBS9rdHrbP6g7BG4_kPP9XV1vCiVewU",
+//   authDomain: "blog-web-49668.firebaseapp.com",
+//   projectId: "blog-web-49668",
+//   storageBucket: "blog-web-49668.appspot.com",
+//   messagingSenderId: "561111016179",
+//   appId: "1:561111016179:web:eef336738659e3fbfb0d86"
+// };
+
+// let firebase = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
